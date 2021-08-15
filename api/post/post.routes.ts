2c661fb2e8764f0d4f8f8569361037f6cf1f2198
whitedevil31 <magz3116@gmail.com>
2021-08-15 13:23:44 +0530

    Add new endpoints for post

---
+++ api/post/post.routes.ts
@@ -0,0 +1,55 @@
+import { Router, Request, Response, NextFunction } from "express";
+import adminMiddleware from "../middleware/auth";
+import { postType, ServiceType } from "./post.schema";
+import { userDB } from "../user/user.schema";
+import { workerPost, getPost, getSingleWorkerPost } from "./post.db";
+import { userInterface } from "../user/user.schema";
+import HttpError from "http-errors";
+const router: Router = Router();
+
+router.post(
+  "/api/worker/post",
+  adminMiddleware,
+  async (req: Request, res: Response, next: NextFunction) => {
+    const data = req.body as postType;
+    const user = req.user as userInterface;
+
+    try {
+      if (req.user) {
+        if (req.user.role == "User")
+          throw HttpError(403, "Users are not allowed to post ");
+      }
+      const result = await workerPost(data, user);
+      res.status(201).send(result);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.get(
+  "/api/worker/post/:postId",
+  adminMiddleware,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const result = await getPost(req.params.postId);
+      res.json(result);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+router.get(
+  "/api/worker/get",
+  adminMiddleware,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const user = req.user as userDB;
+      console.log(user);
+      const result = await getSingleWorkerPost(user);
+      res.json(result);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
